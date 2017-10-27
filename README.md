d7ef709da04dceb7f664fa9359c02117588526b7
gillamritt <guramrit_gill@hotmail.com>
2017-10-27 13:25:37 -0700

    oct 27 2017

--- README.md
+++
@@ -1,1 +0,0 @@
-# websurvey for BCIT
---
+++ scripts/client.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var begin = document.getElementById("begin");
+    
+    begin.addEventListener("click", function() {
+        location.href = "/questions";
+    });
+});
